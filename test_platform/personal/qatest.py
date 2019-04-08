dee2e84e70ab0418b30b00195cc455bc9a459e9f
alvinsss <6449694@qq.com>
2019-04-08 14:09:01 +0800

    django-admin startproject test_platform python manage.py startapp personal

---
+++ test_platform/personal/qatest.py
@@ -0,0 +1,7 @@
+#!usr/bin/env python
+# -*- coding:utf-8
+"""
+@author:alvin
+@file: qatest.py
+@time: 2019/04/08
+"""
--- test_platform/personal/views.py
+++ test_platform/personal/views.py
@@ -1,3 +1,11 @@
+#!usr/bin/env python
+# -*- coding:utf-8
+"""
+"""
 from django.shortcuts import render
-
+from django.http import  HttpResponse
 # Create your views here.
+
+
+def qatest(request):
+	return  HttpResponse("qatest")

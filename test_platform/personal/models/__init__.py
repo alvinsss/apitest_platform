b713bd599af50c4a8463fbe0b7518312d5c3856d
alvinsss <6449694@qq.com>
2019-04-09 11:02:24 +0800

    init 指定文件

---
+++ test_platform/personal/models/__init__.py
@@ -0,0 +1,3 @@
+# 指定文件
+from personal.models.project import Project
+from personal.models.module import Module
--- test_platform/personal/models/module.py
+++ test_platform/personal/models/module.py
@@ -1,13 +1,13 @@
 from django.db import models
-
+from personal.models.project import Project
 
 # Create your models here.
 
 
-class Project(models.Model):
-	name = models.CharField(max_length=40, null=False)
+class Module(models.Model):
+	project = models.ForeignKey(Project, on_delete=models.CASCADE)
+	name = models.CharField(max_length=50, null=False)
 	describe = models.TextField(default="")
-	status = models.BooleanField(default=1)
 	create_time = models.DateTimeField(auto_now_add=True)
 
 

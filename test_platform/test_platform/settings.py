7849122399425df19fe5d109e9c1b495fa020768
alvinsss <6449694@qq.com>
2019-04-09 11:39:02 +0800

    修改中文xadmin和时区

--- test_platform/test_platform/settings.py
+++ test_platform/test_platform/settings.py
@@ -116,16 +116,12 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
-
-TIME_ZONE = 'UTC'
-
+LANGUAGE_CODE = 'zh-hans'
+TIME_ZONE = 'Asia/Shanghai'
 USE_I18N = True
-
 USE_L10N = True
-
-USE_TZ = True
-
+# 数据库取本地时间
+USE_TZ = False
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
